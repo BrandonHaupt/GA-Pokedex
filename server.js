4ccac0a24960cafd7632b3a63340c7ad2390bc38
Brandon Haupt <haupt.brandon@gmail.com>
2022-11-15 20:33:08 -0600

    Update 1.3
    - Added Create, Edit, Delete routes

--- server.js
+++ server.js
@@ -14,6 +14,50 @@ app.use('/static', express.static("public"))
 // HOME ROUTE
 app.get('/', (req,res) => res.redirect('/pokemons'))
 
+// INDEX ROUTE
+app.get('/pokemons', (req,res)=> {
+    res.render('index.ejs', 
+        {
+            allPokemons:pokemons   
+        }
+    )
+})
+
+// CREATE A ROUTE - Recieves data from the form on new.ejs and pushes it to the body of our index.ejs
+app.post('/pokemons', (req,res) => {
+    pokemons.push(req.body)
+
+    res.redirect('/pokemons')
+})
+
+// EDIT ROUTE 
+app.get('/pokemons/:id/edit', (req,res) => {
+    res.render('edit.ejs', 
+        {
+            pokemon: pokemons[req.params.id],
+            index: req.params.id
+        }
+    )
+})
+
+
+// DELETE ROUTE 
+app.delete('/pokemons/:id', (req,res) => {
+    res.splice(req.params.id, 1)
+    res.redirect('/pokemons')
+})
+
+
+// SHOW ROUTE Everything else goes above
+app.get(`/pokemons/:id`, (req,res) => {
+    res.render('show.ejs', 
+        {
+            pokemon: pokemons[req.params.id],
+            id: req.params.id
+        }
+    )
+})
+
 
 
 // Allows us to access local server
